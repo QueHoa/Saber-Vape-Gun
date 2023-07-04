91e96da091a2d2e8a09517617236dc63628ae51f
Nguyễn Hoàng Quế Hòa <nhoxcoy2001.pdl@gmail.com>
2023-07-04 08:58:07 +0700

    add my project

---
+++ Vape Gun Saber/Assets/SaberPlay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaberPlay : MonoBehaviour
+{
+    public Renderer renderer;
+    [SerializeField]private float speed;
+    [SerializeField]private float maxHight;
+    private float dissolve = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if(dissolve > -maxHight) dissolve -= Time.deltaTime * speed;
+        }
+        else
+        {
+            if(dissolve <= 0) dissolve += Time.deltaTime * speed;
+        }
+        renderer.material.SetFloat("_dissolveamount", dissolve);
+    }
+}
