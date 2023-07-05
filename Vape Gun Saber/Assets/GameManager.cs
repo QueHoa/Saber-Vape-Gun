7985c7aa3c421cfbb455987c4b3d2298cb31e292
Nguyễn Hoàng Quế Hòa <nhoxcoy2001.pdl@gmail.com>
2023-07-05 08:15:33 +0700

    add my project

--- Vape Gun Saber/Assets/GameManager.cs
+++ Vape Gun Saber/Assets/GameManager.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public Button but3D;
+    public Button butSetting;
+    public Button butZoom;
+    public Button butSaber;
+    public Button butColor;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Vape Gun Saber/Assets/LightSacber/SaberPlay.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaberPlay : MonoBehaviour
+{
+    public Transform[] saber;
+    [SerializeField]private float speed;
+    private float lengthSword = 0;
+    
+    private void Awake()
+    {
+        for (int i = 0; i < saber.Length; i++)
+        {
+            saber[i].localScale = new Vector3(0, 0, 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if(lengthSword < 1) lengthSword += Time.deltaTime * speed;
+        }
+        else
+        {
+            if (lengthSword > 0) lengthSword -= Time.deltaTime * speed;
+            if (lengthSword < 0) lengthSword = 0;
+        }
+        if(saber.Length >= 3)
+        {
+            saber[0].localScale = new Vector3(1, 1, lengthSword);
+            for (int i = 1; i < saber.Length; i++)
+            {
+                saber[i].localScale = new Vector3(1, lengthSword, 1);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < saber.Length; i++)
+            {
+                saber[i].localScale = new Vector3(1, 1, lengthSword);
+            }
+        }     
+    }
+    private void Set3D()
+    {
+            
+    }
+}
